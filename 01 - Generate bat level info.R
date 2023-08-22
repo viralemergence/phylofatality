155156b407e5605141ef343f4e3a161f92876438
Daniel Becker <10813169+danieljbecker@users.noreply.github.com>
2023-08-22 07:41:48 -0500

    revising 01

--- 01 - Generate bat level info.R
+++ 01 - Generate bat level info.R
@@ -1,23 +1,39 @@
+## phylofatality 01_generate species-level CFR
+## danbeck@ou.edu 
 
-library(tidyverse); library(vroom); library(magrittr)
-setwd("C:/Users/cjcar/Desktop/Phylofatality")
+## clean environment & plots
+rm(list=ls()) 
+graphics.off()
+gc()
 
-vir <- vroom("~/Github/virion/Virion/virion.csv.gz")
+## load packages
+library(tidyverse)
+library(vroom)
+library(magrittr)
+
+## load virion
+setwd("/Users/danielbecker/Desktop/GitHub/virion/Virion")
+vir=vroom("virion.csv.gz")
 vir %<>% filter(HostClass == 'mammalia')
 
-cfr1 <- read_csv("loose_data.csv.txt")
-cfr2 <- read_csv("stringent_data.csv.txt")
-cfr <- bind_rows(cfr1, cfr2)
+## load cfr
+setwd("/Users/danielbecker/Desktop/GitHub/phylofatality")
+cfr1=read_csv("loose_data.csv.txt")
+cfr2=read_csv("stringent_data.csv.txt")
+cfr=bind_rows(cfr1, cfr2)
 
+## rename based on CFR average
 cfr %<>% select(SppName_ICTV_MSL2018b, CFR_avg) %>%
   rename(Virus = SppName_ICTV_MSL2018b, CFR = CFR_avg)
 
+## group
 cfr %<>% group_by(Virus) %>% 
   summarize(CFR = 0.01*mean(CFR))
 
+## fix with virion naming
 cfr %<>% mutate(Virus = str_to_lower(Virus))
 
-## Detour: which names don't stick?####
+## name matching
 table(cfr$Virus %in% vir$Virus)
 cfr$Virus[!(cfr$Virus %in% vir$Virus)]
 rec <- c("colorado tick fever virus" = "colorado tick fever coltivirus",
@@ -26,17 +42,49 @@ rec <- c("colorado tick fever virus" = "colorado tick fever coltivirus",
          "severe acute respiratory syndrome-related coronavirus-2" = "severe acute respiratory syndrome-related coronavirus")
 cfr %<>% mutate(Virus = recode(Virus, !!!rec))
 cfr$Virus[str_detect(cfr$Virus,'middle')] <- "middle east respiratory syndrome-related coronavirus"
-#######################################
 
-vir %<>%
-  filter(Virus %in% cfr$Virus) %>%
-  select(Host, Virus) %>% 
+## recheck
+table(cfr$Virus %in% vir$Virus)
+
+## regroup
+cfr %<>% group_by(Virus) %>% 
+  summarize(CFR = mean(CFR))
+
+## trim virion to NCBI resolved
+vdata=vir[(vir$HostNCBIResolved==T & vir$VirusNCBIResolved==T),]
+table(cfr$Virus %in% vir$Virus)
+
+## simplify vdata to cfr viruses
+vdata=vdata[vdata$Virus%in%cfr$Virus,]
+
+## remove missing host
+vdata=vdata[!is.na(vdata$Host),]
+
+## filter virion
+vdata %<>%
+  select(Host, Virus, VirusGenus, VirusFamily) %>% 
   distinct() %>% drop_na()
 
-vir %<>% left_join(cfr) %>%
+## save alternative
+vraw=vdata
+
+## mean/max across all viruses per host for now
+vdata %<>% left_join(cfr) %>%
   group_by(Host) %>% 
   summarize(meanCFR = mean(CFR),
             maxCFR = max(CFR),
             virusesWithCFR = n())
 
-write_csv(vir, "CFRBySpecies.csv")
+# ## group by host and virus family
+# vraw %<>% left_join(cfr) %>%
+#   group_by(Host,VirusFamily) %>% 
+#   summarize(meanCFR = mean(CFR),
+#             maxCFR = max(CFR),
+#             virusesWithCFR = n())
+# 
+# ## from long to wide
+# vwide1=spread(vraw,VirusFamily,meanCFR)
+
+## export
+setwd("/Users/danielbecker/Desktop/GitHub/phylofatality")
+write_csv(vdata, "CFRBySpecies.csv")
