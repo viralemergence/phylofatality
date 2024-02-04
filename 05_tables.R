79b2e367055ec0772a24016371f478ca352b41bf
Caroline Cummings <139808367+carolinecummings@users.noreply.github.com>
2024-02-04 13:56:14 -0600

    Add files via upload

--- 05_tables.R
+++ 05_tables.R
@@ -23,6 +23,7 @@ data=read.csv("PS_data.csv")
 
 #lambda --> "\u03BB"
 
+#make a table for phylogenetic signal 
 data %>%
   summarize(vfamily, dataset, variable, lambda, lambda_lower, lambda_lower_p, 
             lambda_upper, lambda_upper_p) %>%
@@ -33,3 +34,21 @@ data %>%
   kable(align = "ccccclcl", digits = 3) %>%
   kable_styling(font_size = 15) %>%
   add_header_above(c("Pagel's \u03BB" = 8))
+
+
+#make a table for the clades
+setwd("~/Desktop/PCM Class/phylofatality/clean/csv files")
+data=read.csv("pf_riskyclades.csv")
+
+#filter to risky bats in mammal-wide analysis
+bats<- data%>%
+  slice(c(1,3,5,8,9,15,18,25,29))
+
+#make table
+bats%>%
+  summarize(virus, var, taxa)%>%
+  arrange(virus)%>%
+  dplyr::rename("variable"=var)%>%
+  kable(align = "ccl", digits = 3) %>%
+  kable_styling(font_size = 15) %>%
+  add_header_above(c("Risky bat clades: Mammal-wide analysis" =3))
